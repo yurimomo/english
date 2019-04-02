1772302f91c211e30288d4e9874b5ee0a18e88c3
yuriko <yuri715lily_momo@yahoo.co.jp>
2019-04-02 14:33:57 +0900

    sendgrid

--- app/mailers/article_mailer.rb
+++ app/mailers/article_mailer.rb
@@ -3,7 +3,7 @@ class ArticleMailer < ApplicationMailer
 		@article = article
 		mail(
 			subject: 'read and loud at least 3 times!!',
-			to: 'user@example',
+			to: 'yuri715lily_momo@yahoo.co.jp',
 			from: 'article@example.com')
 	end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,20 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.action_mailer.default_url_options = { host: 'https://strawberry-pudding-77001.herokuapp.com' }
+
+   ActionMailer::Base.delivery_method = :smtp
+   ActionMailer::Base.smtp_settings =
+  {
+   user_name: ENV['SENDGRID_USERNAME'],
+   password: ENV['SENDGRID_PASSWORD'],
+   domain: "heroku.com",
+   address: "smtp.sendgrid.net",
+   port: 587,
+   authentication: :plain,
+   enable_starttls_auto: true
+}
+
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
