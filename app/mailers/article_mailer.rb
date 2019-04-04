cf6bf42a60b4c94d35a487d3b9c84db6d7e7b783
yuriko <yuri715lily_momo@yahoo.co.jp>
2019-04-04 13:49:34 +0900

    mergeAction

--- app/mailers/article_mailer.rb
+++ app/mailers/article_mailer.rb
@@ -7,12 +7,4 @@ class ArticleMailer < ApplicationMailer
 			to: 'yuri715lily_momo@yahoo.co.jp',
 			from: 'article@example.com')
 	end
-
-	def every_article(article)
-		@article = article
-		mail(
-			subject: 'read and loud at least 3 times!!',
-			to: 'yuri715lily_momo@yahoo.co.jp',
-			from: 'article@example.com')
-	end
 end
